064a85dc3faf67a4f005555cafcadfb0ce33017b
Ilya Amursky <ilya.amursky@gmail.com>
2023-05-23 17:53:12 +0200

    script no block

--- scripts/create-service.sh
+++ scripts/create-service.sh
@@ -4,4 +4,4 @@ cd scripts
 cp ./pawxy-test-assignment.service /etc/systemd/system/pawxy-test-assignment.service
 systemctl daemon-reload
 systemctl enable pawxy-test-assignment
-systemctl start pawxy-test-assignment
+systemctl start pawxy-test-assignment --no-block
